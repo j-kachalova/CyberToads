563d680e48df8ad21adb5dc2a8d492999f308f95
Aiden Izumi <m.kozhinov@haulmont.com>
2021-12-04 05:24:15 +0400

    Add usage scenarios section

--- front-app/src/Components/Content/MainPage/MainPage.jsx
+++ front-app/src/Components/Content/MainPage/MainPage.jsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { animateScroll as scroll, scroller } from "react-scroll";
 import {Advantages} from "../advantages/Advantages";
+import {UsageScenarios} from "../usage_scenarios/UsageScenarios";
 
 const MainPage = () => {
 
@@ -33,6 +34,7 @@ const MainPage = () => {
             <div className='Header_container' name='MainBanner' />
             <MainBanner />
             <Advantages />
+            <UsageScenarios/>
             <div className='testbox' name='section2'/>
         </div>
     )
